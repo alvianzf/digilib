764852ed597fa6b3f41907c8c40b46f42e018f53
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2020-01-20 13:45:11 +0700

    data summary

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -17,6 +17,8 @@ class Dashboard extends MY_Controller
         if (!@$this->session->userdata['is_logged_in']) {
             redirect('/auth', 'refresh');
         }
+
+        $this->load->model(['user_model', 'buku_model', 'history_model']);
     }
 
 
@@ -25,6 +27,14 @@ class Dashboard extends MY_Controller
      */
     public function index()
     {
-        
+        $this->data['jumlah_pengguna']  = $this->user_model->count_by('role', 'user');
+        $this->data['jumlah_buku']      = $this->buku_model->count_by('id', !0);
+        $this->data['jumlah_unduhan']   = $this->history_model->count_by('id', !0);
+
+        $user_data = $this->session->userdata['user_detail']->user_data[0];
+
+        $this->data['nama']             = $user_data->nama;
+        $this->data['last_download']    = date('d M Y', $this->history_model->order_by('id', 'desc')->get_by('user_id', $user_data->user_id)->created_at);
+        $this->data['total_download']  = $this->history_model->count_by('user_id', $user_data->user_id);
     }
 }
---
+++ application/views/dashboard/index.php
@@ -0,0 +1,53 @@
+<h1><i class="fas fa-book text-info"></i> Digital Library SMKN 3 Tanjungpinang</h1>
+
+<hr />
+
+<div class="row">
+    <div class="col-md-4">
+        <div class="card">
+            <div class="card-header">
+                <h5>Ringkasan sistem</h5>
+            </div>
+            <div class="card-body">
+                <table width="100%" class="table table-collapse table-condensed">
+                    <tr>
+                        <td>Pengguna</td>
+                        <td style="text-align: right;" class="text-info"><?= $jumlah_pengguna ?></td>
+                    </tr>
+                    <tr>
+                        <td>Koleksi Buku</td>
+                        <td style="text-align: right;" class="text-info"><?= $jumlah_buku ?></td>
+                    </tr>
+                    <tr>
+                        <td>Total Unduhan</td>
+                        <td style="text-align: right;" class="text-info"><?= $jumlah_unduhan ?></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">
+                <h5>Ringkasan Saya</h5>
+            </div>
+            <div class="card-body">
+                <table width="100%" class="table table-collapse table-condensed">
+                    <tr>
+                        <td>Nama Pengguna</td>
+                        <td style="text-align: right;" class="text-primary"><?= $nama ?></td>
+                    </tr>
+                    <tr>
+                        <td>Unduhan Terakhir</td>
+                        <td style="text-align: right;" class="text-primary"><?= $last_download ?></td>
+                    </tr>
+                    <tr>
+                        <td>Total Unduhan</td>
+                        <td style="text-align: right;" class="text-primary"><?= $total_download ?></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
